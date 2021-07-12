89a4220067011fc4c601cd43ee5e0efba94e4681
KCLin <winjoexd@gmail.com>
2021-07-12 13:57:45 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,12 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+#![feature(abi_x86_interrupt)]
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
+pub mod gdt;
 use core::panic::PanicInfo;
 
 pub trait Testable {
@@ -42,6 +45,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
@@ -66,4 +70,9 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
         let mut port = Port::new(0xf4);
         port.write(exit_code as u32);
     }
+}
+
+pub fn init() {
+    gdt::init();
+    interrupts::init_idt();
 }
